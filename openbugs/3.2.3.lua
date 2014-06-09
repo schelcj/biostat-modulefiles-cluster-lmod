3ca7b22a4c8aa92ae842ce26210a3b4fe367cbd0
Chris Scheller <schelcj@pobox.com>
2014-06-09 10:47:21 -0400

    just adding another module

---
+++ openbugs/3.2.3.lua
@@ -0,0 +1,18 @@
+help([[
+BUGS is a software package for performing Bayesian inference Using Gibbs Sampling.
+]])
+
+whatis("Description: BUGS is a software package for performing Bayesian inference Using Gibbs Sampling.")
+whatis("Vendor: http://www.openbugs.info/w/")
+whatis("Licenses: N/A")
+
+local app     = "openbugs"
+local version = "3.2.3"
+local modroot = pathJoin("/home/software/trusty", app, version)
+
+conflict(app)
+
+prepend_path("PATH",               pathJoin(modroot, "bin"))
+prepend_path("MANPATH",            pathJoin(modroot, "share", "man"))
+prepend_path("LD_LIBRARY_PATH",    pathJoin(modroot, "lib"))
+prepend_path("LIBRARY_PATH",       pathJoin(modroot, "lib"))
