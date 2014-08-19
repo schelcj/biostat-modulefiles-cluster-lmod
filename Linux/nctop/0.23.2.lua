c5b3f8e9988824641e0e0c4a8619629e55e4046a
Chris Scheller <schelcj@pobox.com>
2014-08-19 10:22:52 -0400

    ooops, need to make that path automatic

--- Linux/nctop/0.23.2.lua
+++ Linux/nctop/0.23.2.lua
@@ -8,7 +8,7 @@ whatis("Licenses: N/A")
 
 local app     = "nctop"
 local version = "0.23.2"
-local modroot = pathJoin("/home/software", app)
+local modroot = pathJoin("/home/software/trusty", app)
 
 conflict(app)
 
