5f36e1d02772037a524ebaf48713da4e51f6aa91
Chris Scheller <schelcj@pobox.com>
2014-08-19 11:14:21 -0400

    haven't upgrade to 9.3 yet and the paths are different from standard.

--- Linux/sas/9.2.lua
+++ Linux/sas/9.2.lua
@@ -7,10 +7,10 @@ whatis("Vendor: http://www.sas.com/")
 whatis("Licenses: unlimited")
 
 local app     = "SAS"
-local version = "9.3"
-local modroot = pathJoin("/home/software/trusty", app, version)
+local version = "9.2"
+local modroot = pathJoin("/home/software", app)
 
 conflict(app)
 
-prepend_path("PATH", pathJoin(odroot, "SASFoundation/" .. version .. "/bin"))
+prepend_path("PATH", pathJoin(modroot, "SASFoundation/" .. version .. "/bin"))
 prepend_path("PATH", pathJoin(modroot, "SASFoundation/" .. version))
