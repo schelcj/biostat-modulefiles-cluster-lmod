a9a29371db1ada0ef2ea2fc76bcba26583f3a9a0
Chris Scheller <schelcj@pobox.com>
2014-10-01 09:30:40 -0400

    fixup the MANPATH

--- Core/StdEnv.lua
+++ Core/StdEnv.lua
@@ -1,5 +1,12 @@
 local software_base = "/home/software"
+
 prepend_path("BIOSTAT_SOFTWARE_BASE", software_base)
 prepend_path("BIOSTAT_SOFTWARE", pathJoin(software_base, capture("lsb_release -s -c")))
+
+-- lmod is mucking about with MANPATH somewhere that i can't find
+-- so just set it back to normal
+prepend_path("MANPATH", "/usr/local/share/man")
+prepend_path("MANPATH", "/usr/local/man")
+prepend_path("MANPATH", "/usr/share/man")
 
 load("PrivateModules", "biostat", "slurm", "R", "perl")
