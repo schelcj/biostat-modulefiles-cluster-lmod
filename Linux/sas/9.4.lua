97e78d5122a6063ae067924422b1629a34eaa02a
Chris Scheller <schelcj@pobox.com>
2014-10-07 15:45:39 -0400

    new version of SAS

---
+++ Linux/sas/9.4.lua
@@ -0,0 +1,16 @@
+help([[
+SAS Statistical Analysis package
+]])
+
+whatis("Description: SAS Statistical Analysis package")
+whatis("Vendor: http://www.sas.com/")
+whatis("Licenses: unlimited")
+
+local app     = "sas"
+local version = "9.4"
+local modroot = pathJoin(os.getenv("BIOSTAT_SOFTWARE"), app, version)
+
+conflict(app)
+
+prepend_path("PATH", pathJoin(modroot, "SASFoundation/" .. version .. "/bin"))
+prepend_path("PATH", pathJoin(modroot, "SASFoundation/" .. version))
